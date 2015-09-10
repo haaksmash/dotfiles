fad6a6aec1551cc3b49e14c0079370829c53100a
Haak Saxberg <haak@zenpayroll.com>
2015-09-10 14:58:41 -0700

    gusto specific changes

--- zsh-files/exports.zsh
+++ zsh-files/exports.zsh
@@ -6,3 +6,12 @@ export VENVS_DIR=$HOME/.virtualenvs
 
 # Vim is Life, Vim is Happiness
 export EDITOR=vim
+
+# ZP stuff
+export STE_DRB_URI="druby://localhost:3777"
+export RUBY_GC_HEAP_INIT_SLOTS=600000
+export RUBY_GC_MALLOC_LIMIT=59000000
+export RUBY_HEAP_FREE_MIN=100000
+
+export ZPDEBUG_LIVE_EDIT=1
+export ZPDEBUG_LIVE_EDIT_PORT=1337
